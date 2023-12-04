c5c9d4a7f86e3dd49146def0c5279c3f105f610e
Nataliia Smalchenko <137941820+lana-daalang@users.noreply.github.com>
2023-12-04 20:59:05 +0200

    resolved second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// var throttle = require('lodash.throttle');
+
+player.on(
+  'timeupdate',
+  throttle(evt => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(evt.seconds)
+    );
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') ?? 0);
