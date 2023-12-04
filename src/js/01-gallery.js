2e00b2a66f3085bc935bf36bf9ba8b420b511602
Nataliia Smalchenko <137941820+lana-daalang@users.noreply.github.com>
2023-12-04 20:18:40 +0200

    resolved first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+function createMarkup(galleryItems) {
+  const galleryList = [];
+
+  for (const { preview, original, description } of galleryItems) {
+    galleryList.push(`
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+      </li>`);
+  }
+  return galleryList.join('');
+}
+
+function openImage(event) {
+  const img = event.target;
+  if (img.nodeName !== 'IMG') {
+    return;
+  }
+  event.preventDefault();
+}
+
+const gallery = document.querySelector('ul.gallery');
+
+gallery.innerHTML = createMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.addEventListener('click', openImage);
